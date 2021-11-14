cd473d713614131bdc34e6cdb216abb83a233a90
Sam <samcpollock@gmail.com>
2021-11-14 18:47:39 -0500

    Adding Gameroom functionality
    - Following along with Week 5, adding gamerooms

--- Assets/NetworkedServer.cs
+++ Assets/NetworkedServer.cs
@@ -15,11 +15,19 @@ public class NetworkedServer : MonoBehaviour
     int socketPort = 5491;
 
     LinkedList<PlayerAccount> playerAccounts;
+    LinkedList<GameRoom> gameRooms;
+
+    string playerAccountFilePath;
+
+    int playerWaitingForMatch = -1;
 
 
     // Start is called before the first frame update
     void Start()
     {
+
+        playerAccountFilePath = (Application.dataPath + Path.DirectorySeparatorChar + "PlayerAccountData.txt");
+
         NetworkTransport.Init();
         ConnectionConfig config = new ConnectionConfig();
         reliableChannelID = config.AddChannel(QosType.Reliable);
@@ -28,6 +36,9 @@ public class NetworkedServer : MonoBehaviour
         hostID = NetworkTransport.AddHost(topology, socketPort, null);
 
         playerAccounts = new LinkedList<PlayerAccount>();
+        gameRooms = new LinkedList<GameRoom>();
+
+        LoadPlayerAccounts();
     }
 
     // Update is called once per frame
@@ -97,6 +108,8 @@ public class NetworkedServer : MonoBehaviour
                 PlayerAccount pa = new PlayerAccount(name, pass);
                 playerAccounts.AddLast(pa);
                 SendMessageToClient(ServerToClientSignifiers.CreateAccountSuccess + "", id);
+                
+                SavePlayerAccounts();
             }
             else // Checking if signifier was something else.
             {
@@ -137,6 +150,85 @@ public class NetworkedServer : MonoBehaviour
 
         }
 
+        else if (signifier == ClientToServerSignifiers.AddToGameRoomQueue)
+        {
+            if (playerWaitingForMatch == -1)    // is the only waiting player
+            {
+                playerWaitingForMatch = id;
+            }
+            else    // Start a game session
+            {
+
+                GameRoom gr = new GameRoom(playerWaitingForMatch, id);
+                gameRooms.AddLast(gr);
+                SendMessageToClient(ServerToClientSignifiers.GameRoomStarted + "", id);
+                SendMessageToClient(ServerToClientSignifiers.GameRoomStarted + "", playerWaitingForMatch);
+
+
+                playerWaitingForMatch = -1;    // Reset to having nobody waiting for match
+            }
+        }
+
+        else if (signifier == ClientToServerSignifiers.TicTacToePlay)
+        {
+            Debug.Log("SENT A PLAY");
+
+            GameRoom gr = FindGameRoomWithPlayerID(id);
+
+            if (gr.playerID1 == id)
+            {
+                SendMessageToClient(ServerToClientSignifiers.OpponentPlayed + "", gr.playerID2);
+            }
+            else
+            {
+                SendMessageToClient(ServerToClientSignifiers.OpponentPlayed + "", gr.playerID1);
+
+            }
+        }
+
+
+    }
+
+    private void SavePlayerAccounts()
+    {
+        StreamWriter sw = new StreamWriter(playerAccountFilePath);
+        foreach (PlayerAccount pa in playerAccounts)
+        {
+            sw.WriteLine(pa.name + "," + pa.password);
+        }
+
+        sw.Close();
+    }
+
+    private void LoadPlayerAccounts()
+    {
+        if (File.Exists(playerAccountFilePath))
+        {
+
+            StreamReader sr = new StreamReader(playerAccountFilePath);
+
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] csv = line.Split(',');
+
+                PlayerAccount pa = new PlayerAccount(csv[0], csv[1]);
+
+                playerAccounts.AddLast(pa);
+            }
+        }
+    }
+
+    private GameRoom FindGameRoomWithPlayerID(int id)
+    {
+        foreach(GameRoom gr in gameRooms)
+        {
+            if (gr.playerID1 == id || gr.playerID2 == id)
+            {
+                return gr;
+            }
+        }
+        return null;
     }
 
 }
@@ -154,6 +246,18 @@ public class PlayerAccount
     }
 }
 
+public class GameRoom
+{
+    // Hold two clients. 
+    public int playerID1, playerID2;
+
+    public GameRoom(int PlayerID1, int PlayerID2)
+        {
+        playerID1 = PlayerID1;
+        playerID2 = PlayerID2;
+        }
+}
+
 
 public static class ServerToClientSignifiers
 {
@@ -163,11 +267,18 @@ public static class ServerToClientSignifiers
     public const int CreateAccountFailure = 3;
     public const int LoginFailure = 4;
 
+    public const int GameRoomStarted = 5;
+
+    public const int OpponentPlayed = 6;
+
 }
 
 public static class ClientToServerSignifiers
 {
     public const int CreateAccountAttempt = 1;
     public const int LoginAttempt = 2;
+    public const int AddToGameRoomQueue = 3;
+    public const int TicTacToePlay = 4;
+
 
 }
